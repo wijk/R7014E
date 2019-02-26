ff9d226d016c91e12a8d502df83f1d9cbb6cdc70
Oskar Wijkström <oskwij-4@student.ltu.se>
2019-02-26 18:02:16 +0100

    i want to die

--- Lab A/R7014E_lib_R003/myTank.m
+++ Lab A/R7014E_lib_R003/myTank.m
@@ -6,7 +6,7 @@ h_eq = 3.2;
 h1 = h_1.signals.values(end);
 h2 = h_2.signals.values(end);
 q_in = q.signals.values(end);
-R = [1 0; 0 1];
+R = [0.033333 0; 0 0.33333];
 Q = [1];
 Ts = 0.01;
 D = 0;
@@ -16,11 +16,11 @@ a_1 = ao_1*sqrt(2*g)/2*A_a;
 a_2 = ao_2*sqrt(2*g)/2*A_a;
 
 
-A = [a_1*(-1/sqrt(2*g*(h1+h_eq))) 0 ; 
-    a_1*(1/sqrt(2*g*(h1+h_eq))) a_2*(-1/sqrt(2*g*(h2+h_eq)))];
+A = [a_1*(-1/sqrt(2*g*(h1+EqPoint))) 0 ; 
+    a_1*(1/sqrt(2*g*(h1+EqPoint))) a_2*(-1/sqrt(2*g*(h2+EqPoint)))];
 
-A_2 = [-0.309 0;    
-        0.309 -0.309];
+A_2 = [-0.998 0;    
+        1.5306e-4 -0.998];
 
 B = [6.95/36.2; 0];
 C = [0 1];
